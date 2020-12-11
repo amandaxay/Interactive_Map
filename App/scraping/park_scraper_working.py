f525cf0b7189e8564158623da2592588fe42fdef
FenrisAkashi <blackflames713@yahoo.com>
2020-12-10 20:59:12 -0800

    Added states to csv file to show which state each park was scraped for

--- App/scraping/park_scraper_working.py
+++ App/scraping/park_scraper_working.py
@@ -25,13 +25,13 @@ for state in list: #Remember to update the number of pages
         if i.find('div', class_='col-md-9 col-sm-9 col-xs-12 table-cell list_left') == None:
             break
         park=i.find('div', class_='col-md-9 col-sm-9 col-xs-12 table-cell list_left').find('h3').text.strip()
-        print('Park:',park)
+        #print('Park:',park)
         location=i.find('div', class_='col-md-9 col-sm-9 col-xs-12 table-cell list_left').find('h4').text.strip()
-        print('Location:', location)
+        #print('Location:', location)
         description=i.find('div', class_='col-md-9 col-sm-9 col-xs-12 table-cell list_left').find('p').text.strip()
-        print('Description:', description)
+        #print('Description:', description)
         
-        database.append((park,location,description))
+        database.append((park,state,location,description))
     
-df = pd.DataFrame(database, columns=['park','location','description'])
+df = pd.DataFrame(database, columns=['park','state','location','description'])
 df.to_csv('data/national_parks.csv', index=False, encoding='utf-8')
