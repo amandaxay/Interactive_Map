43f6c42edd196a91f0e0397d874be7c9e94989ea
Amanda <hi.amanda.s@gmail.com>
2020-12-13 17:58:40 -0800

    Journal Entries using pure JavaScript

--- App/journal.js
+++ App/journal.js
@@ -1,42 +1,48 @@
-var today = new Date();
-var expiry = new Date(today.getTime() + 30 * 24 * 3600 * 1000); // plus 30 days
+String.prototype.isEmpty = function(){
+  return (this.length === 0 || !this.trim());
+};
 
-function setCookie(name, value)
-{
-  document.cookie=name + "=" + escape(value) + "; path=/; expires=" + expiry.toUTCString();
+
+var guestMessages = {
+  title: [],
+  msgs: [],
+
+  getMessages: function(msg){
+    this.title.push(value1 + "<br>");
+    this.msgs.push("<br>"+ value);
+  },
+
+  printMessages: function(){
+    guestMessages.getMessages();
+    var showTitle = document.getElementById("titleoutput").innerHTML = this.title.join(" ");
+    var showEntry = document.getElementById("entry").innerHTML = this.msgs.join(" ");
+  }
+};
+
+//console.log(keepmsgs);
+
+var getEntry = function(){
+  value1 = document.getElementById("title").value;
+  value = document.getElementById("content").value;
+  return value1 + value;
+};
+
+
+var clearDiv = function(){
+  document.getElementById("title").value = "";
+  document.getElementById("content").value = "";
 }
 
-function storeValues(form)
-{
-  setCookie("field1", form.getElementById("field1").value);
-  setCookie("field2", form.getElementById("field2").value);
-  return true;
+var isEmpty = function(entry){
+  entry = getEntry();
+  var output = document.getElementById("content").value;
+  var output2 = document.getElementById("title").value;
+  if ( output === '' || output2 === '' ){
+    alert("Incomplete fields! Fill in all boxes");
+  }
+  else {
+    guestMessages.printMessages();
+  }
 }
 
-function getCookie(name)
-{
-  var re = new RegExp(name + "=([^;]+)");
-  var value = re.exec(document.cookie);
-  return (value != null) ? unescape(value[1]) : null;
-}
-
-
-const expired = new Date(today.getTime() - 24 * 3600 * 1000); // less 24 hours
-
-function deleteCookie(name)
-{
-  document.cookie=name + "=null; path=/; expires=" + expired.toUTCString();
-}
-
-function clearCookies()
-{
-    deleteCookie("field1");
-    deleteCookie("field2");
-    alert('Your cookies have been deleted!');
-}
-
-function display()
-{
-  document.write(getCookie("field1"));
-  document.write(getCookie("field2"));
-}
+// updateEntry();
